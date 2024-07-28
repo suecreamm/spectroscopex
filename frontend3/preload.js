f922d93ade0a16dabd82f84bc0c664977ca42f2c
suecream <100839777+suecreamm@users.noreply.github.com>
2024-07-29 01:20:04 +0900

    Delete frontend3 directory

--- frontend3/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-const fs = require('fs');
-const Papa = require('papaparse');
-
-contextBridge.exposeInMainWorld('myAPI', {
-  readFile: (filePath) => fs.readFileSync(filePath, 'utf8'),
-  parseCSV: (data) => Papa.parse(data, { header: true, dynamicTyping: true })
-});
